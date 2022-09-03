0ab05944fe92037efaf5093c6313217105e22c81
CrlsDev <c6099020@gamil.com>
2022-09-02 19:58:37 -0500

    Inicio

--- src/actividadformulas/ActividadFormulas.java
+++ src/actividadformulas/ActividadFormulas.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package actividadformulas;
 
 /**
  *
- * @author david
+ * @authors
+ * Carlos Manuel Cardenas Toledo
+ * Johan Sebastian García Triana
+ * Carlos David Romero Restrepo
  */
 public class ActividadFormulas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // ª
     }
     
 }
